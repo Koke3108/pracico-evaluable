4263916fdc0afe62585b08adab65cd0d5a9017e6
Jorge <joliva@sipadan.es>
2024-11-08 11:48:39 +0100

    Terminado

--- web/routes/index.js
+++ web/routes/index.js
@@ -5,45 +5,12 @@ var datos = require("../data/dataprovider");
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) { 
-  res.render('home', {head_title: "Principal"});
+router.get('/', function (req, res, next) {
+  res.render('home', { head_title: "Principal" });
 });
 
 
-router.get('/celebraciones', function(req, res, next) {
-  res.redirect("/celebraciones.html");
-});
-
-
-router.get('/contacta-con-nosotros', function(req, res, next) {
-  res.render("contacto", {head_title: "Contacto"});
-});
-
-router.get('/contact-with-us', function(req, res, next) {
-  res.redirect("/contacto.html");
-});
-
-router.get('/galeria', function(req, res, next) {
-  const imagenes = datos.getGalleryData()
-  res.render("galeria",{head_title:"Galerias de imágenes", imagenes:imagenes});
-});
-
-router.get('/carta', function(req,res){
-  const carta = datos.getAllCarta();
-  res.render("carta",{carta:carta});
-});
-
-router.get('/carta/:id',function(req,res){
-  const plato = datos.getItemCarta(req.params.id);
-  res.send(plato);
-});
-
-router.get('/index',function(req,res){
-  res.render("index",{title:"Bootstrap"});
-});
-
-
-router.get('/debug/:category/:id', function(req, res, next) {
+router.get('/debug/:category/:id', function (req, res, next) {
   console.log(req.body);
   console.log(req.hostname);
   console.log(req.params);
